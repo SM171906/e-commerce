30a7aa6beea441b9c12519747380d777b1a65603
Smiti <mishra.smiti06@gmail.com>
2022-01-31 15:23:09 -0500

    nested component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainContent from "./MainContent";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+  <React.Fragment>
+    <NavBar />
+    <MainContent />
+  </React.Fragment>
+  )
+}
+
+export default App;
---
+++ src/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent() {
+  return(
+    <div>
+      Hello there!!
+    </div>
+  )
+}
+
+export default MainContent;
